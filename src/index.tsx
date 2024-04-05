deee670a34b1e8484e917d758ef2591e5ee98316
Chris Serino <themindoverall@gmail.com>
2024-04-05 08:57:24 -0500

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import { Hono } from 'hono'
+import { renderer } from './renderer'
+
+const app = new Hono()
+
+app.use(renderer)
+
+app.get('/', (c) => {
+  return c.render(<h1>Hello!</h1>)
+})
+
+export class Room {
+  async fetch() {
+    return new Response("Hello world")
+  }
+}
+
+export default app
